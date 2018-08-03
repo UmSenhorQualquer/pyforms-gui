34f2a17f5092365fe9fb00c0086b82647a46493c
Ricardo Ribeiro <ricardojvr@gmail.com>
2018-08-03 17:44:47 +0100

    Add fixes done on v3

--- pyforms_gui/controls/control_matplotlib.py
+++ pyforms_gui/controls/control_matplotlib.py
@@ -20,11 +20,11 @@ from pyforms_gui.controls.control_base import ControlBase
 from AnyQt import _api
 
 if _api.USED_API == _api.QT_API_PYQT5:
-	from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-	from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+    from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+    from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 elif _api.USED_API == _api.QT_API_PYQT4:
-	from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
-	from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
+    from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+    from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
 
 from matplotlib.figure import Figure
 from mpl_toolkits.mplot3d import Axes3D
@@ -33,59 +33,61 @@ from mpl_toolkits.mplot3d import Axes3D
 
 class ControlMatplotlib(ControlBase, QWidget):
 
-	def __init__(self, *args, **kwargs):
-		QWidget.__init__(self)
-		ControlBase.__init__(self, *args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        QWidget.__init__(self)
+        ControlBase.__init__(self, *args, **kwargs)
 
-	def init_form(self):
+        self.on_draw = kwargs.get('on_draw', self.on_draw) 
 
-		self._fig   = Figure((5.0, 4.0), dpi=100)
-		self.canvas = FigureCanvas(self._fig)
-		self.canvas.setParent(self)
-		self.mpl_toolbar = NavigationToolbar(self.canvas, self)
-	 
-		vbox = QVBoxLayout()
-		vbox.addWidget(self.canvas)
-		vbox.addWidget(self.mpl_toolbar)
-		self.setLayout(vbox)
+    def init_form(self):
 
-	@property
-	def value(self): return None
+        self._fig   = Figure((5.0, 4.0), dpi=100)
+        self.canvas = FigureCanvas(self._fig)
+        self.canvas.setParent(self)
+        self.mpl_toolbar = NavigationToolbar(self.canvas, self)
+     
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.canvas)
+        vbox.addWidget(self.mpl_toolbar)
+        self.setLayout(vbox)
 
-	@value.setter
-	def value(self, value): 
-		self.on_draw = value
-		self.draw()
+    @property
+    def value(self): return None
 
-	def draw(self): 
-		self.on_draw(self._fig)
-		self.canvas.draw()
+    @value.setter
+    def value(self, value): 
+        self.on_draw = value
+        self.draw()
 
-	def on_draw(self, figure):
-		""" Redraws the figure
-		"""
-		x = range(len(self.value))
+    def draw(self): 
+        self.on_draw(self._fig)
+        self.canvas.draw()
 
-		#self._axes = self._fig.add_subplot(111)
-		
-		#self._axes.bar(left=x, height=self.data)
-		#self.canvas.draw()
+    def on_draw(self, figure):
+        """ Redraws the figure
+        """
+        x = range(len(self.value))
 
-		axes = figure.add_subplot(111, projection='3d')
-		axes.clear(); 
-		pts = axes.scatter(x, x, x, c=x)
-		figure.colorbar(pts)
+        #self._axes = self._fig.add_subplot(111)
+        
+        #self._axes.bar(left=x, height=self.data)
+        #self.canvas.draw()
 
-
-	############################################################################
-	############ Properties ####################################################
-	############################################################################
-
-	@property
-	def fig(self): return self._fig
-	@fig.setter
-	def fig(self, value): self._fig = value
+        axes = figure.add_subplot(111, projection='3d')
+        axes.clear(); 
+        pts = axes.scatter(x, x, x, c=x)
+        figure.colorbar(pts)
 
 
-	@property
-	def form(self): return self
+    ############################################################################
+    ############ Properties ####################################################
+    ############################################################################
+
+    @property
+    def fig(self): return self._fig
+    @fig.setter
+    def fig(self, value): self._fig = value
+
+
+    @property
+    def form(self): return self
