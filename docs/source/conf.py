097dbc33159beb0e5e59a06d73be748a4550b848
Ricardo Ribeiro <ricardojvr@gmail.com>
2018-08-10 15:46:45 +0100

    Update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -160,7 +160,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'PyformsTerminal.tex', 'Pyforms Terminal Documentation',
+    (master_doc, 'PyformsGUI.tex', 'Pyforms GUI Documentation',
      'Ricardo Jorge Vieira Ribeiro', 'manual'),
 ]
 
@@ -170,7 +170,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'pyformsweb', 'Pyforms Terminal Documentation',
+    (master_doc, 'pyformsgui', 'Pyforms GUI Documentation',
      [author], 1)
 ]
 
@@ -181,8 +181,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'PyformsTerminal', 'Pyforms Terminal Documentation',
-     author, 'PyformsTerminal', 'One line description of project.',
+    (master_doc, 'PyformsGUI', 'Pyforms GUI Documentation',
+     author, 'PyformsGUI', 'One line description of project.',
      'Miscellaneous'),
 ]
 
