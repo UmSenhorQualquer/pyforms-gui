f1fcf76118116ef06c785033bd2e8a86428ef189
Ricardo Ribeiro <ricardojvr@gmail.com>
2018-10-30 15:26:12 +0000

    update version

--- pyforms_gui/__init__.py
+++ pyforms_gui/__init__.py
@@ -4,7 +4,7 @@
 __author__      = "Ricardo Ribeiro"
 __credits__     = ["Ricardo Ribeiro"]
 __license__     = "MIT"
-__version__     = "4.0.8"
+__version__     = "4.0.9"
 __maintainer__  = "Ricardo Ribeiro"
 __email__       = "ricardojvr@gmail.com"
 __status__      = "Development"
